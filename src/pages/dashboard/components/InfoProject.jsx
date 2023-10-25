db2b9c9f0d4b3eff05790f339fdfbadffc4ef793
sara040616 <sara.sierraramos@gmail.com>
2023-10-25 14:45:25 -0500

    Petición para campo de descripción y status

--- src/pages/dashboard/components/InfoProject.jsx
+++ src/pages/dashboard/components/InfoProject.jsx
@@ -3,23 +3,73 @@ import { useParams } from 'react-router-dom';
 
 function InfoProject() {
   const { id } = useParams();
-  const [project, setProject] = useState(null);
+  // const [status, setStatus] = useState('Not Started')
 
+  const options = [
+    { label: 'Not Started', value: 'Not Started' },
+    { label: 'On Track', value: 'On Track' },
+    { label: 'Off Track', value: 'Off Track' },
+    { label: 'Done', value: 'Done' },
+  ]
+
+  const [project, setProject] = useState(null);
   useEffect(() => {
     fetch(`https://dev-api.focalpoint.nearshoretc.com/project/${id}`)
       .then((response) => response.json())
       .then((data) => setProject(data));
   }, [id]);
 
+
+  const addObjetives = () => {
+
+    fetch(`https://dev-api.focalpoint.nearshoretc.com/project/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Actualizada----------:', data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  function handleSelect(event) {
+    setProject({ ...project, status: event.target.value })
+  }
+
   return (
     <div>
       {project ? (
         <div>
           <h2>{project.project_name}</h2>
+          <p>Descripción: {project.project_description}</p>
+          <input
+            type="text"
+            value={project.project_description}
+            onChange={(e) => setProject({ ...project, project_description: e.target.value })}
+          />
+
           <h1>{id}</h1>
+          <div className='d-flex flex-row-reverse '>
+            <div className='w-25 p-3 border rounded'>
+              <h4>Status</h4>
+              <select className='form-select' onChange={handleSelect}>
+                {options.map((option, index) => (
+                  <option selected={options.value === project.status} value={option.value} key={index}>{option.label}</option>
+                ))}
+              </select>
+              <p>{project.status}</p>
+            </div>
+
+          </div>
+
+          <button onClick={addObjetives}>Agregar</button>
         </div>
       ) : (
-        <div>Cargando datos del proyecto...</div>
+        <div>Cargando</div>
       )}
     </div>
   );
