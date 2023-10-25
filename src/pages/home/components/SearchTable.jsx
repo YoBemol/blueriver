48c8be2c60392ecce66a85be228fff52b056ef6c
RisellU <rcum22@gmail.com>
2023-10-25 16:39:23 -0500

    paginacion

--- src/pages/home/components/SearchTable.jsx
+++ src/pages/home/components/SearchTable.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
 import TableProjects from './TableProjects';
 import SearchBar from '../../../components/Search';
-
+import Pagination from './paginacion';
 
 function SearchTable() {
   const [users, setUsers] = useState(null);
   const [filteredUsers, setFilteredUsers] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const projectsPerPage = 10;
+
 
   const getUrl = async () => {
     await fetch("https://dev-api.focalpoint.nearshoretc.com/project")
@@ -22,16 +25,28 @@ function SearchTable() {
       return e.project_name.toString().toLowerCase().includes(query.toLowerCase());
     });
     setFilteredUsers(resultSearch);
+    setCurrentPage(1);
   }
 
   useEffect(() => {
     getUrl();
   }, []);
 
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = filteredUsers?.slice(indexOfFirstProject, indexOfLastProject);
+
   return (
     <div className='search'>
       <SearchBar onSearch={handleSearch} />
-      <TableProjects users={filteredUsers} />
+      <TableProjects users={currentProjects} />
+      {filteredUsers && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(filteredUsers.length / projectsPerPage)}
+          onPageChange={setCurrentPage}
+        />
+      )}
     </div>
   );
 }
