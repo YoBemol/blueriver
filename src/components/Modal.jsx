30452915dc8d49fd9b56605c3ad3f24cb185a828
Katherine Encalada <katherineencalada@Katherines-MacBook-Air.local>
2023-10-25 09:22:42 -0500

    modal useState

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,30 @@
-import './modal.css'
+import { useState } from "react";
+import "./modal.css";
 
-const Modal = () => {
-    return (
+const Modal = ({ estado, cambiarEstado, item, updateData }) => {
+    console.log(item)
+    const [description, setDescription] = useState(item?.description)
+
+  return (
+    <>
+      {estado && 
         <div className="modalContainer">
-            <div className="modalContent">
-                <h3>Escribe tu Milestone</h3>
-                <input type="text" className='modalInput' placeholder='Escribe aquí' />
-                <span className='closeModal'>&times;</span>
-            </div>
+          <div className="modalContent">
+            <h3>Escribe tu Milestone</h3>
+            <input
+              type="text"
+              className="modalInput"
+              placeholder="Escribe aquí"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <button onClick={() => updateData(description)}>Enviar</button>
+            <span className="closeModal" onClick={cambiarEstado}>&times;</span>
+          </div>
         </div>
-    )
-}
+      }
+    </>
+  );
+};
 
 export default Modal;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -37,26 +37,31 @@ const tableData = [
         description: "description initiation",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:1
       },
       {
         description: "description initiation",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:2
       },
       {
         description: "description initiation",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:3
       },
       {
         description: "description initiation",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:4
       },
       {
         description: "description initiation",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:5
       },
     ],
   },
@@ -67,26 +72,31 @@ const tableData = [
         description: "description planning",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:1
       },
       {
         description: "description planning",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:2
       },
       {
         description: "description planning",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:3
       },
       {
         description: "description planning",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:4
       },
       {
         description: "description planning",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:5
       },
     ],
   },
@@ -97,26 +107,31 @@ const tableData = [
         description: "description execution",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:1
       },
       {
         description: "description execution",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:2
       },
       {
         description: "description execution",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:3
       },
       {
         description: "description execution",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:4
       },
       {
         description: "description execution",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:5
       },
     ],
   },
@@ -127,26 +142,31 @@ const tableData = [
         description: "description monitoring",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:1
       },
       {
         description: "description monitoring",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:2
       },
       {
         description: "description monitoring",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:3
       },
       {
         description: "description monitoring",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:4
       },
       {
         description: "description monitoring",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:5
       },
     ],
   },
@@ -157,26 +177,31 @@ const tableData = [
         description: "description closure",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:1
       },
       {
         description: "description closure",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:2
       },
       {
         description: "description closure",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:3
       },
       {
         description: "description closure",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:4
       },
       {
         description: "description closure",
         plannedDate: "01/01/2020",
         currentDate: "01/01/2020",
+        id:5
       },
     ],
   },
@@ -187,21 +212,43 @@ function Dashboard() {
   const handlePhaseClick = (id) => {
     setActivePhase(id);
   };
+  const [estadoModal1, cambiarEstadoModal1] = useState(false);
+  const [data, setData] = useState(tableData)
+  const [modalItem, setModalItem] = useState(null)
+  const handleModal = (item) => {
+    setModalItem(item)
+    cambiarEstadoModal1(true)
+
+  }
+
+  const handleUpdate = (description) => {
+    const tempData = [...data]
+    tempData[activePhase].items.map((single) => {
+      if (single.id === modalItem.id) {
+        single.description=description
+      }
+      return single
+    })
+    setData(tempData)
+    cambiarEstadoModal1(false)
+  }
 
   return (
     <div>
-      <Modal></Modal>
+      {estadoModal1 &&
+      <Modal estado={estadoModal1} cambiarEstado={() => cambiarEstadoModal1(false)} item={modalItem} updateData={(description) => handleUpdate(description)}></Modal>
+      }
       <div className="layout">
         <div className="leftSection">
           <div className="dashboardContainer">
             <SideBar />
           </div>
         </div>
-        <div className="rigthSection">
-          <section>
+        <div className="rightSection">
+          <section className="searchSection">
             <Search />
           </section>
-          <section></section>
+          <section className="informationCharged"></section>
           <section className="phasesContainer">
             {Phases.map((phase, index) => {
               return (
@@ -214,11 +261,17 @@ function Dashboard() {
               );
             })}
           </section>
-          <section style={{ width: "50%" }}>
-            <Table
-              headers={["Description", "Planned Date", "Current Date"]}
-              data={tableData[activePhase]}
-            ></Table>
+          <section className="tablesContainer" /*style={{ width: "50%" }}*/>
+            <section className="leftTable">
+              <Table
+                headers={["Description", "Planned Date", "Current Date"]}
+                data={data[activePhase]}
+                handleClick={(item) => handleModal(item)}
+              ></Table>
+            </section>
+            <section className="rightTable">
+
+            </section>
           </section>
         </div>
       </div>
