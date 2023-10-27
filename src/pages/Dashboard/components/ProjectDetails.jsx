4696239f32881f32fde3f0df2d93652be4ea7209
sara040616 <sara.sierraramos@gmail.com>
2023-10-27 14:49:26 -0500

    Agregando comentarios

--- src/pages/Dashboard/components/ProjectDetails.jsx
+++ src/pages/Dashboard/components/ProjectDetails.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 
 function ProjectDetails({ project }) {
+  // Estado para almacenar la fase seleccionada
   const [selectedPhase, setSelectedPhase] = useState(null);
+  // Estado para almacenar el tipo de "Key Update" seleccionado
   const [selectedKeyUpdateType, setSelectedKeyUpdateType] = useState(null);
 
+  // Función para manejar el clic en una fase
   const handlePhaseClick = (phaseName) => {
     setSelectedPhase(phaseName);
     setSelectedKeyUpdateType(null); // Reiniciamos el tipo de "Key Update" seleccionado
-
   };
 
+  // Función para manejar el clic en un tipo de "Key Update"
   const handleKeyUpdateTypeClick = (keyUpdateType) => {
     setSelectedKeyUpdateType(keyUpdateType);
   };
-
 
   return (
     <div>
@@ -24,6 +26,7 @@ function ProjectDetails({ project }) {
         <h3>Fases:</h3>
         <ul>
           {Object.keys(project.milestones).map((phaseName) => (
+            // Itera sobre las fases y muestra una lista de ellas.
             <li
               key={phaseName}
               onClick={() => handlePhaseClick(phaseName)}
@@ -43,6 +46,7 @@ function ProjectDetails({ project }) {
           <h3>Tipos de Key Updates para {selectedPhase}:</h3>
           <ul>
             {Object.keys(project.key_updates[selectedPhase]).map((keyUpdateType) => (
+              // Itera sobre los tipos de "Key Updates" y muestra una lista de ellos.
               <li
                 key={keyUpdateType}
                 onClick={() => handleKeyUpdateTypeClick(keyUpdateType)}
@@ -63,9 +67,9 @@ function ProjectDetails({ project }) {
           <h3>{selectedKeyUpdateType}:</h3>
           <ul>
             {project.key_updates[selectedPhase][selectedKeyUpdateType].map((keyUpdate) => (
+              // Itera sobre los valores de "Key Update" para un tipo específico y muestra una lista de ellos.
               <li
                 key={keyUpdate.key_update_id}
-
               >
                 {keyUpdate.key_update_value}
               </li>
@@ -73,7 +77,6 @@ function ProjectDetails({ project }) {
           </ul>
         </div>
       )}
-
     </div>
   );
 }
