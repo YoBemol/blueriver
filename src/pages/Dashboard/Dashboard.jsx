d1a7b33b170ccbbd702bda1d302c2d473c3861c3
sara040616 <sara.sierraramos@gmail.com>
2023-10-28 12:37:02 -0500

    Estilo para la paginación

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,81 +5,20 @@
 // import "./dashboard.css";
 // import Search from "../../components/Search";
 // import Modal from "../../components/Modal";
+import SideBar from '../../components/SideBar';
 import InfoProject from './components/InfoProject';
 
 
 
 function Dashboard() {
-//   const [activePhase, setActivePhase] = useState(0);
-//   const handlePhaseClick = (id) => {
-//     setActivePhase(id);
-//   };
-//   const [estadoModal1, cambiarEstadoModal1] = useState(false);
-//   // const [data, setData] = useState(tableData)
-//   const [modalItem, setModalItem] = useState(null)
-//   const handleModal = (item) => {
-//     setModalItem(item)
-//     cambiarEstadoModal1(true)
-
-//   }
-
-//   // const handleUpdate = (description) => {
-//   //   const tempData = [...data]
-//   //   tempData[activePhase].items.map((single) => {
-//   //     if (single.id === modalItem.id) {
-//   //       single.description=description
-//   //     }
-//   //     return single
-//   //   })
-//   //   setData(tempData)
-//   //   cambiarEstadoModal1(false)
-//   // }
 
    return (
-//     <div>
-//       {estadoModal1 &&
-//       <Modal estado={estadoModal1} cambiarEstado={() => cambiarEstadoModal1(false)} item={modalItem} updateData={(description) => handleUpdate(description)}></Modal>
-//       }
-//       <div className="layout">
-//         <div className="leftSection">
-//           <div className="dashboardContainer">
-//             <SideBar />
-//           </div>
-//         </div>
-//         <div className="rightSection">
-//           <section className="searchSection">
-//             <Search />
-//           </section>
-//           <section className="informationCharged">
-//           <InfoProject/>
-//           </section>
-//           <section className="phasesContainer">
-//             {Phases.map((phase, index) => {
-//               return (
-//                 <Tabs
-//                   key={index}
-//                   name={phase.name}
-//                   active={index === activePhase}
-//                   handleClick={() => handlePhaseClick(index)}
-//                 />
-//               );
-//             })}
-//           </section>
-//           <section className="tablesContainer" /*style={{ width: "50%" }}*/>
-//             <section className="leftTable">
-//               <Table
-//                 headers={["Description", "Planned Date", "Current Date"]}
-//                 data={data[activePhase]}
-//                 handleClick={(item) => handleModal(item)}
-//               ></Table>
-//             </section>
-//             <section className="rightTable">
-                <InfoProject />
-//             </section>
-//           </section>
-//         </div>
-//       </div>
-//     </div>
+      <div className='layout'>
+         <SideBar/>
+         <InfoProject />
+      </div>
+
+
    );
- }
- export default Dashboard;
+}
+export default Dashboard;
